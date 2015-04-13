7b80803b429b7b94599fe31160cafbad6a92e38d
AugustM94 <augustmbius@gmail.com>
2015-04-13 14:36:04 +0200

    Klassenavn

--- ProjectManagement/src/soen/management/app/ManagementApp.java
+++ ProjectManagement/src/soen/management/app/ManagementApp.java
@@ -2,7 +2,7 @@ package soen.management.app;
 
 import soen.management.app.User;
 
-public class Main {
+public class ManagementApp {
 	public static User user;
 	
 	public static void main(String[] args) {
