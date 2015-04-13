50050a2a4b99a43d02380b53c218da853700e87c
Meincke91 <meincke91@hotmail.com>
2015-04-13 15:33:33 +0200

    minor changes user

--- ProjectManagement/src/soen/management/app/ManagementApp.java
+++ ProjectManagement/src/soen/management/app/ManagementApp.java
@@ -7,13 +7,9 @@ public class ManagementApp {
 	public static User user2;
 	
 	public static void main(String[] args) {
-
 		user = new User("Niki");
 		user2 = new User("Martin");
 		System.out.println(user2.getUserID());
-		//test
-
-
 
 	}
 }
