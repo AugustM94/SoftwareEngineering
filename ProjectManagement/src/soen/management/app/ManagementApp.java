200cad8ab5d20544407a0f5bb5ac72603b26dba4
AugustM94 <augustmbius@gmail.com>
2015-04-13 15:54:15 +0200

    Project Class added

--- ProjectManagement/src/soen/management/app/ManagementApp.java
+++ ProjectManagement/src/soen/management/app/ManagementApp.java
@@ -7,6 +7,7 @@ public class ManagementApp {
 	public static User user2;
 	
 	public static void main(String[] args) {
-
+		User user = new User("Nikki");
+		System.out.println(user.getUserID());
 	}
 }
