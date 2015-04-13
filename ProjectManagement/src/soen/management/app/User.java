3e88bf5af7103beeb178946c4dcec1f955df035b
AugustM94 <augustmbius@gmail.com>
2015-04-13 15:15:34 +0200

    Minor changes user

--- ProjectManagement/src/soen/management/app/User.java
+++ ProjectManagement/src/soen/management/app/User.java
@@ -6,21 +6,22 @@ public class User {
 	private int userID;
 	private static int nextID = 1;
 	private String name;
-	private Activity activties[];
+	private int activityID[];
 	private int Hours[][];
 	
 	
 	public User(String name){
 		this.name = name;
-		ArrayList<Activity> activites = new ArrayList<Activity>();
+		ArrayList<Integer> activityID = new ArrayList<Integer>();
 		this.userID = nextID;
 		nextID++;
 	}
 	
 	public int getUserID(){
-		String id = ""+userID;
-		id = id.format("%03d",userID); //Adds zeros in front of string.
-		return Integer.parseInt(id);
+		Integer id = userID;
+		String format = "%07d";
+		String result = String.format(format,id); //Adds zeros in front of string.
+		return Integer.parseInt(result);
 	}
 
 	public String getName(){
