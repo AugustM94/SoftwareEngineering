e9f31b78054c0aa8fab5bd15672365c2cb47213d
Meincke91 <meincke91@hotmail.com>
2015-04-20 14:46:12 +0200

    working file reader

--- ProjectManagement/src/soen/management/app/FileReader.java
+++ ProjectManagement/src/soen/management/app/FileReader.java
@@ -3,10 +3,56 @@ package soen.management.app;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-
 import java.io.IOException;
  
 
 public class FileReader {
+/*
+	Scanner input;
+	
+	input = new Scanner(new File(""));
+	while (input.hasNext()){
+		String something = (String)input.nextLine();
+		System.out.println(something);
+	}
+	*/
+    public static void main(String[] args) {
+    	
+    }
+    
+    public String getFileData(){
+    	System.out.println("did shit");
+        File file = new File("src/hej.txt");
 
+        BufferedReader br = null;
+
+        String line = "";
+        
+        try {
+            java.io.FileReader fr = new java.io.FileReader(file);
+            br = new BufferedReader(fr);
+
+            
+
+            while( (line = br.readLine()) != null ) {
+                System.out.println(line);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + file.toString());
+        } catch (IOException e) {
+            System.out.println("Unable to read file: " + file.toString());
+        }
+        finally {
+            try {
+                br.close();
+            } catch (IOException e) {
+                System.out.println("Unable to close file: " + file.toString());
+            }
+            catch(NullPointerException ex) {
+            }
+        }
+        
+    	return line;
+    }
 }
--- ProjectManagement/src/soen/management/app/ManagementApp.java
+++ ProjectManagement/src/soen/management/app/ManagementApp.java
@@ -1,24 +1,16 @@
 package soen.management.app;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 public class ManagementApp {
 	public static User user;
 	public static User user2;
+	static FileReader fileReader = new FileReader();
 	
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) {
 
 		User user = new User("Nicki");
 		//System.out.println(user.getUserID());
-		
-		Scanner input;
-		
-		input = new Scanner(new File("src/hej.txt"));
-		while (input.hasNext()){
-			String something = (String)input.nextLine();
-			System.out.println(something);
-		}
+		fileReader.getFileData();
+		System.out.println("did this shit");
 	}
+	
 }
