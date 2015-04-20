4a204161131c04eb33be4d839735acd8608bd8e4
Niki94 <niki.hald@gmail.com>
2015-04-20 14:23:33 +0200

    NonProjectActivities class

--- ProjectManagement/src/soen/management/app/NonProjectActivities.java
+++ ProjectManagement/src/soen/management/app/NonProjectActivities.java
@@ -1,5 +1,17 @@
 package soen.management.app;
 
 public class NonProjectActivities {
-
+	private int NonProjectActivitiesID;
+	private String name;
+	private int startDate;
+	private int endDate;
+	private int timeStamp;
+	private boolean active;
+	
+	
+	public NonProjectActivities(String name){
+		this.name = name;
+		this.NonProjectActivitiesID = 1;
+		this.timeStamp = (int) (System.currentTimeMillis() / 1000L);
+	}
 }
