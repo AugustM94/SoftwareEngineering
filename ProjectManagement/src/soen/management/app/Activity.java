8e54479a67e586e557a08e3f25cd48db8d0a526f
Niki94 <niki.hald@gmail.com>
2015-04-20 13:54:49 +0200

    Activity class

--- ProjectManagement/src/soen/management/app/Activity.java
+++ ProjectManagement/src/soen/management/app/Activity.java
@@ -2,4 +2,22 @@ package soen.management.app;
 
 public class Activity {
 
+	private int activityID; 
+	private int projectID;
+	private String name;
+	private int startDate;
+	private int endDate;
+	private int AssignedUsers[];
+	private int HoursSpendPerUser[][];
+	private int timeStamp;
+	private boolean active;
+	
+	public Activity(String name, int projectID){
+		this.name = name;
+		this.activityID = 1;
+		this.projectID = 1;
+		this.timeStamp = (int) (System.currentTimeMillis() / 1000L);
+	}
+	
+	
 }
