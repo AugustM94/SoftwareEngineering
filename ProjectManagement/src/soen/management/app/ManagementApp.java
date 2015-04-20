38139e8a138576e74235d6226203037a5a6af03d
Meincke91 <meincke91@hotmail.com>
2015-04-20 14:34:31 +0200

    semi-functional scanner

--- ProjectManagement/src/soen/management/app/ManagementApp.java
+++ ProjectManagement/src/soen/management/app/ManagementApp.java
@@ -1,48 +1,24 @@
 package soen.management.app;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import soen.management.app.User;
+import java.util.Scanner;
 
 public class ManagementApp {
 	public static User user;
 	public static User user2;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 
 		User user = new User("Nicki");
-		System.out.println(user.getUserID());
+		//System.out.println(user.getUserID());
 		
-        File file = new File("hej.txt");
-        
-        BufferedReader br = null;
-
-        try {
-            java.io.FileReader fr = new java.io.FileReader(file);
-            br = new BufferedReader(fr);
-
-            String line;
-
-            while( (line = br.readLine()) != null ) {
-                System.out.println(line);
-            }
-
-        } catch (FileNotFoundException e) {
-            System.out.println("File not found: " + file.toString());
-        } catch (IOException e) {
-            System.out.println("Unable to read file: " + file.toString());
-        }
-        finally {
-            try {
-                br.close();
-            } catch (IOException e) {
-                System.out.println("Unable to close file: " + file.toString());
-            }
-            catch(NullPointerException ex) {
-            }
-        }
+		Scanner input;
+		
+		input = new Scanner(new File("src/hej.txt"));
+		while (input.hasNext()){
+			String something = (String)input.nextLine();
+			System.out.println(something);
+		}
 	}
 }
