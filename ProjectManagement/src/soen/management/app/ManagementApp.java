bfaa732a24aca0b003c2d552b37e5fb679cdafa8
Meincke91 <meincke91@hotmail.com>
2015-04-27 14:08:46 +0200

    again

--- ProjectManagement/src/soen/management/app/ManagementApp.java
+++ ProjectManagement/src/soen/management/app/ManagementApp.java
@@ -9,9 +9,26 @@ public class ManagementApp {
 	static FileReader fileReader = new FileReader();
 	
 	public static void main(String[] args) {
+<<<<<<< Updated upstream
 
 		User user = new User("Nicki");
 		//System.out.println(user.getUserID());
+=======
+<<<<<<< HEAD
+<<<<<<< HEAD
+		//User user = new User("Nicki");
+		loadProjects();
+=======
+
+		User user = new User("Nicki");
+		//System.out.println(user.getUserID());
+>>>>>>> origin/master
+=======
+
+		User user = new User("Nicki");
+		//System.out.println(user.getUserID());
+>>>>>>> origin/master
+>>>>>>> Stashed changes
 		
 		
 		//JSONObject jsonObject = new JSONObject(fileReader.getFileData());
